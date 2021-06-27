4c470a2766db153177f3bf931dfbfdbca9aefe7b
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-06-27 11:09:53 +0200

    datanode nodemanager

--- p1/hadoop_install.sh
+++ p1/hadoop_install.sh
@@ -33,7 +33,6 @@ exit
 docker container commit hadoop-install hadoop-base
 docker images
 docker container rm hadoop-install
-
 docker network create hadoop-cluster
 docker container run -ti --name namenode --network=hadoop-cluster --hostname namenode --net-alias resourcemanager --expose 8000-10000 -p 9870:9870 -p 8088:8088 hadoop-base /bin/bash
 mkdir -p /var/data/hdfs/namenode
@@ -251,3 +250,224 @@ exit
 docker container commit namenode namenode-image
 docker images
 docker container rm namenode
+docker container run -ti --name datanode --network=hadoop-cluster --hostname datanode --expose 8000-10000 --expose 50000-50200 hadoop-base /bin/bash
+mkdir -p /var/data/hdfs/datanode
+su - hdadmin
+
+hadoop_conf_backup_folder="hadoop_conf_backup"
+mkdir "$hadoop_conf_backup_folder"
+cp "$HADOOP_HOME/etc/hadoop/core-site.xml" \
+  "$HADOOP_HOME/etc/hadoop/hdfs-site.xml" \
+  "$HADOOP_HOME/etc/hadoop/yarn-site.xml" \
+  "$HADOOP_HOME/etc/hadoop/mapred-site.xml" \
+  "$hadoop_conf_backup_folder"
+
+hadoop_conf_file="core-site.xml"
+head -n -1 "$hadoop_conf_backup_folder/$hadoop_conf_file" > "$HADOOP_HOME/etc/hadoop/$hadoop_conf_file"
+echo '  <property>
+    <!-- Nombre del filesystem por defecto -->
+    <!-- Como queremos usar HDFS tenemos que indicarlo con hdfs:// y el servidor y puerto en el que corre el NameNode -->
+    <name>fs.defaultFS</name>
+    <value>hdfs://namenode:9000/</value>
+    <final>true</final>
+  </property>
+  <property>
+    <!-- Directorio para almacenamiento temporal (debe tener suficiente espacio) -->
+    <name>hadoop.tmp.dir</name>
+    <value>/var/tmp/hadoop-${user.name}</value>
+    <final>true</final>
+  </property>
+</configuration>' >> "$HADOOP_HOME/etc/hadoop/$hadoop_conf_file"
+
+hadoop_conf_file="hdfs-site.xml"
+head -n -1 "$hadoop_conf_backup_folder/$hadoop_conf_file" > "$HADOOP_HOME/etc/hadoop/$hadoop_conf_file"
+echo '  <property>
+    <!-- Lista (separada por comas) de directorios donde los DataNodes guardan los bloques HDFS -->
+    <name>dfs.datanode.data.dir</name>
+    <value>file:///var/data/hdfs/datanode</value>
+    <final>true</final>
+  </property>
+ </configuration>' >> "$HADOOP_HOME/etc/hadoop/$hadoop_conf_file"
+
+hadoop_conf_file="yarn-site.xml"
+head -n -1 "$hadoop_conf_backup_folder/$hadoop_conf_file" > "$HADOOP_HOME/etc/hadoop/$hadoop_conf_file"
+echo '  <property>
+    <!-- Nombre del equipo que ejecuta el demonio ResourceManager -->
+    <name>yarn.resourcemanager.hostname</name>
+    <value>resourcemanager</value>
+    <final>true</final>
+  </property>
+
+  <property>
+    <!-- Activa la auto-deteccion de las capacidades de los nodos (memoria y CPU) -->
+    <name>yarn.nodemanager.resource.detect-hardware-capabilities</name>
+    <value>true</value>
+    <final>true</final>
+  </property>
+
+  <property>
+    <!-- Número de vcores  que pueden alocarse para contenedores -->
+    <!-- Si vale -1, se detecta automáticamente (si la auto-deteccion está activada) -->
+    <name>yarn.nodemanager.resource.cpu-vcores</name>
+    <value>-1</value>
+    <final>true</final>
+  </property>
+
+  <property>
+    <!-- MB de RAM física que puede ser reservada para los containers (por defecto: 8192) -->
+    <!-- debe ser menor que la RAM fisica, para que funcionen otros servicios -->
+    <!-- Si vale -1, se detecta automáticamente (si la auto-deteccion está activada) -->
+    <!-- Se puede cambiar por un valor fijo, p.e. 3072 (3 GB) -->
+    <name>yarn.nodemanager.resource.memory-mb</name>
+    <value>-1</value>
+    <final>true</final>
+  </property>
+
+  <property>
+    <!-- Deshabilita el chequeo de los límites de uso de la memoria virtual -->
+    <name>yarn.nodemanager.vmem-check-enabled</name>
+    <value>false</value>
+    <final>true</final>
+  </property>
+
+
+ <property>
+    <!-- Indica a los NodeManagers que tienen que implementar el servicio de barajado MapReduce -->
+    <name>yarn.nodemanager.aux-services</name>
+    <value>mapreduce_shuffle</value>
+    <final>true</final>
+  </property>
+
+  <property>
+    <!-- Clase que implementa el servicio de barajado MapReduce -->
+    <name>yarn.nodemanager.aux-services.mapreduce_shuffle.class</name>
+    <value>org.apache.hadoop.mapred.ShuffleHandler</value>
+    <final>true</final>
+  </property>
+
+</configuration>' >> "$HADOOP_HOME/etc/hadoop/$hadoop_conf_file"
+
+hadoop_conf_file="mapred-site.xml"
+head -n -1 "$hadoop_conf_backup_folder/$hadoop_conf_file" > "$HADOOP_HOME/etc/hadoop/$hadoop_conf_file"
+echo '  <property>
+    <!-- Framework que realiza el MapReduce -->
+    <name>mapreduce.framework.name</name>
+    <value>yarn</value>
+    <final>true</final>
+  </property>
+
+  <!-- Configuracion del ApplicationMaster (AM) del MR -->
+
+  <property>
+    <!-- Localizacion del software MR para el AM -->
+    <name>yarn.app.mapreduce.am.env</name>
+    <value>HADOOP_MAPRED_HOME=${HADOOP_HOME}</value>
+  </property>
+
+  <property>
+    <!-- Numero maximo de cores para el ApplicationMaster (por defecto: 1) -->
+    <name>yarn.app.mapreduce.am.resource.cpu-vcores</name>
+    <value>1</value>
+    <final>true</final>
+  </property>
+
+  <property>
+    <!-- Memoria que necesita el ApplicationMaster del MR (por defecto: 1536) -->
+    <name>yarn.app.mapreduce.am.resource.mb</name>
+    <value>1536</value>
+    <final>true</final>
+  </property>
+
+
+  <!-- Configuracion de los maps y reduces del MR -->
+
+  <property>
+    <!-- Ratio del tamaño del heap al tamaño del contenedor para las JVM (por defecto: 0.8)-->
+    <name>mapreduce.job.heap.memory-mb.ratio</name>
+    <value>0.8</value>
+    <final>true</final>
+  </property>
+
+  <!-- Maps -->
+  <property>
+    <!-- Localizacion del software MR para los maps -->
+    <name>mapreduce.map.env</name>
+    <value>HADOOP_MAPRED_HOME=${HADOOP_HOME}</value>
+  </property>
+
+  <property>
+    <!-- Numero maximo de cores para cada tarea map (por defecto: 1) -->
+    <name>mapreduce.map.cpu.vcores</name>
+    <value>1</value>
+    <final>true</final>
+  </property>
+
+  <property>
+    <!-- Opciones para las JVM de los maps -->
+    <name>mapreduce.map.java.opts</name>
+    <value>-Xmx1024M</value> <!-- Xmx define el tamaño máximo de la pila de Java -->
+    <final>true</final>
+  </property>
+
+  <property>
+    <!-- Memoria maxima (MB) por map (si -1 se optiene a partir de mapreduce.map.java.opts y mapreduce.job.heap.memory-mb.ratio) -->
+    <name>mapreduce.map.memory.mb</name>
+    <value>-1</value>
+    <final>true</final>
+  </property>
+
+  <!-- Reduces -->
+  <property>
+    <!-- Localizacion del software MR para los reducers -->
+    <name>mapreduce.reduce.env</name>
+    <value>HADOOP_MAPRED_HOME=${HADOOP_HOME}</value>
+  </property>
+
+  <property>
+    <!-- Numero maximo de cores para cada tarea reduce (por defecto: 1) -->
+    <name>mapreduce.reduce.cpu.vcores</name>
+    <value>1</value>
+    <final>true</final>
+  </property>
+
+  <property>
+    <!-- Opciones para las JVM de los reduces -->
+    <name>mapreduce.reduce.java.opts</name>
+    <value>-Xmx2048M</value> <!-- Xmx define el tamaño máximo de la pila de Java -->
+    <final>true</final>
+  </property>
+
+  <property>
+    <!-- Memoria maxima (MB) por reduce (si -1 se optiene a partir de mapreduce.map.java.opts y mapreduce.job.heap.memory-mb.ratio) -->
+    <name>mapreduce.reduce.memory.mb</name>
+    <value>-1</value>
+    <final>true</final>
+  </property>
+
+</configuration>' >> "$HADOOP_HOME/etc/hadoop/$hadoop_conf_file"
+
+hdfs --daemon start datanode
+cat $HADOOP_HOME/logs/*
+jps
+yarn --daemon start nodemanager
+cat $HADOOP_HOME/logs/*
+jps
+yarn --daemon stop nodemanager
+hdfs --daemon stop datanode
+exit
+echo '#!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_HOME=/opt/bd/hadoop
+
+# Inicio el DataNode y el NodeManager
+su hdadmin -c "$HADOOP_HOME/bin/hdfs --daemon start datanode"
+su hdadmin -c "$HADOOP_HOME/bin/yarn --daemon start nodemanager"
+
+# Lazo para mantener activo el contenedor
+while true; do sleep 10000; done' > /inicio.sh
+chmod +x /inicio.sh
+exit
+docker container commit datanode datanode-image
+docker images
+docker container rm datanode
+
