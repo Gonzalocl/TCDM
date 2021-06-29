555711e9aae91a91463d0a5f48ab550725728a28
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-06-29 11:47:53 +0200

    p2p1

--- p2/p1.sh
+++ p2/p1.sh
@@ -1,2 +1,19 @@
 
 # Not intended to be run as scripts if not running each command one by one
+
+# download FileSystemCat.zip
+unzip FileSystemCat.zip
+docker cp FileSystemCat namenode:/home/luser
+docker container exec -ti namenode /bin/bash
+chown -R luser:luser /home/luser/FileSystemCat
+su - luser
+echo "aaaa
+bbbb
+cccc
+dddd" > text_file.txt
+hdfs dfs -put text_file.txt .
+cd FileSystemCat
+mvn package
+cd target; hadoop jar hdfs-0.0.1-SNAPSHOT.jar text_file.txt
+exit
+exit
