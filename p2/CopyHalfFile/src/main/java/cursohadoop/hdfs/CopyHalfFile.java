cadf53a35ad82b3800340c9d2c7c262b9d1ecc34
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-06-29 12:09:51 +0200

    open and cloes

--- p2/CopyHalfFile/src/main/java/cursohadoop/hdfs/CopyHalfFile.java
+++ p2/CopyHalfFile/src/main/java/cursohadoop/hdfs/CopyHalfFile.java
@@ -23,27 +23,30 @@ public class CopyHalfFile {
 		// Entrada
 		Path pathin = new Path(src);
 		// TODO: Crear un FileSystem para la entrada y un FSDataInputStream para leer los datos
-		FileSystem fsin = FileSystem.get();
-		FSDataInputStream fin = ;
+		FileSystem fsin = FileSystem.get(URI.create(src),  conf);
+		FSDataInputStream fin = null;
 
 		// Salida
 		Path pathout = new Path(dst);		
 		// TODO: Crear un FileSystem para la salida y un FSDataInputStream para escribir los datos
-		FileSystem fsout = FileSystem.get();
-		FSDataOutputStream fout = ;
+		FileSystem fsout = FileSystem.get(URI.create(dst),  conf);
+		FSDataOutputStream fout = null;
 		
 		try {
 			// TODO: Crear un FileStatus del filesystem de entrada a partir del cual obtener la longitud
 
-			
-			// TODO: Abrir los FSDataInputStream y FSDataOutputStream
 
-			
+			// TODO: Abrir los FSDataInputStream y FSDataOutputStream
+			fin = fsin.open(pathin);
+			fout = fsout.open(pathout);
+
 			// TODO: Saltar a la mitad de InputStream de entrada
 			
 			// TODO: Copiar del InputStream al OutputStream
 		} finally {
 			// TODO: Cerrar los streams
+			IOUtils.closeStream(fin);
+			IOUtils.closeStream(fout);
 
 		}		
 	}
---
+++ p2/p2.sh
@@ -0,0 +1,15 @@
+
+# Not intended to be run as scripts if not running each command one by one
+
+# download CopyHalfFile.zip
+unzip CopyHalfFile.zip
+# edit CopyHalfFile
+docker cp CopyHalfFile namenode:/home/luser
+docker container exec -ti namenode /bin/bash
+chown -R luser:luser /home/luser/CopyHalfFile
+su - luser
+cd CopyHalfFile
+mvn package
+cd target; hadoop jar hdfs-0.0.1-SNAPSHOT.jar text_file.txt
+exit
+exit
