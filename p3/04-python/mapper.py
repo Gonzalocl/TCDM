ec289642a68c48e457b1aeb65846cc31a912a346
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-07-01 23:43:05 +0200

    if continue

--- p3/04-python/mapper.py
+++ p3/04-python/mapper.py
@@ -5,4 +5,6 @@ for line in sys.stdin:
     fields = line[:-1].split(',')
     country = fields[4]
     claims = fields[8]
+    if country == '"COUNTRY"':
+        continue
     print('{}\t{}'.format(country, claims))
