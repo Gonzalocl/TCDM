c26052a5e34217ad639c103e1cde9415d7b12cd6
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2021-07-01 21:21:08 +0200

    mapper

--- p3/04-python/mapper.py
+++ p3/04-python/mapper.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import sys
 
 for line in sys.stdin:
-    print('{}\t{}'.format(0, line))
+    fields = line.strip().split(',')
+    country = fields[4]
+    claims = fields[8]
+    print('{}\t{}'.format(country, claims))
